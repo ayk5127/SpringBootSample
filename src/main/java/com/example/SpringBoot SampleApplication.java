87b24885ea64cca6d93e30cc929ea2bf38ea6214
ayk5127 <ayk5127.chirol2@gmail.com>
2025-07-06 23:09:00 +0900

    ログイン画面およびユーザー登録画面の作成

---
+++ src/main/java/com/example/SpringBoot SampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUserInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUserInfoApplication.class, args);
+	}
+
+}
