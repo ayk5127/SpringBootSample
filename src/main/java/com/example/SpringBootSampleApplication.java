5fbf0fc8237c70d333eb2d982b45a439a20ef78f
ayk5127 <ayk5127.chirol2@gmail.com>
2025-07-07 10:55:55 +0900

    誤記修正

--- src/main/java/com/example/SpringBootSampleApplication.java
+++ src/main/java/com/example/SpringBootSampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootUserInfoApplication {
+public class SpringBootSampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootUserInfoApplication.class, args);
+		SpringApplication.run(SpringBootSampleApplication.class, args);
 	}
 
 }
