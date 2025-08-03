a93977ee0af69c8370105f92ab2ff888935f1b24
ayk5127 <ayk5127.chirol2@gmail.com>
2025-08-03 22:16:56 +0900

    バリデーション（入力チェック）の実装

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.application.service.UserApplicationService;
+import com.example.form.GroupOrder;
 import com.example.form.SignupForm;
 
 import lombok.extern.slf4j.Slf4j;
@@ -27,7 +29,8 @@ public class SignupController {
 	
 	/** ユーザー登録画面を表示 */
 	@GetMapping("/signup")
-	public String getSignup(Model model, Locale locale, @ModelAttribute SignupForm form) {
+	public String getSignup(Model model, Locale locale, 
+			@ModelAttribute SignupForm form) {
 		// 性別を取得
 		Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
 		model.addAttribute("genderMap", genderMap);
@@ -38,7 +41,9 @@ public class SignupController {
 	
 	/** ユーザー登録処理 */
 	@PostMapping("/signup")
-	public String postSignup(Model model, Locale locale, @ModelAttribute SignupForm form, BindingResult bindingResult) {
+	public String postSignup(Model model, Locale locale, 
+			@ModelAttribute @Validated(GroupOrder.class) SignupForm form, 
+			BindingResult bindingResult) {
 		
 		// 入力チェック結果
 		if (bindingResult.hasErrors()) {
